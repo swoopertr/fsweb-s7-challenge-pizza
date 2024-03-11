1aea6cce1d590a2ccda87a08d16d26203ff85237
gokalpcbn <gokalpcoban00@gmail.com>
2024-03-11 22:13:33 +0300

    secondcommit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import BackGroundImage from "../../../Assets/mile1-assets/home-banner.png";
+
+const Home = () => {
+  return (
+    <>
+      <div className="text">
+        <h2>Teknolojik Yemekler</h2>
+        <h1>KOD ACIKTIRIR</h1>
+        <h1>PIZZA, DOYURUR</h1>
+        <button>ACIKTIM</button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
