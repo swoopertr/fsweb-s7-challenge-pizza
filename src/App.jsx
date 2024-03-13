34a95a0e2de11c80386eb2260466f3e196f3981c
sybrk <snn.yll@gmail.com>
2024-03-13 20:22:17 +0300

    routing started,

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,27 @@ import reactLogo from "./assets/react.svg";
 import workintech from "/workintech.svg";
 import "./App.css";
 import Home from "./components/home/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/pizzaorder">
+            {/* buraya pizza sipariş formu gelecek. */}
+          </Route>
+        </Switch>
+      </Router>
+
     </>
   );
 }
