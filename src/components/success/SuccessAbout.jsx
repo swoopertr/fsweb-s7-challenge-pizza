bc6acc650b6f0d13c4a199ce7624ff197ba2ae7f
elif ece <eceeliftest@gmail.com>
2024-03-22 12:11:10 +0300

    result düzenlemesi yapıldı

--- src/components/success/SuccessAbout.jsx
+++ src/components/success/SuccessAbout.jsx
@@ -2,17 +2,22 @@ import { useParams } from "react-router-dom";
 import "./SuccessAbout.css";
 
 const SuccessAbout = () => {
-
-    const {pizzaDetails} = useParams()
-    return (
-        <div className="success-about-container">
-            <h2 className="SuccessAboutH2">Pizza İsmi</h2>
-            <div className="success-about-smallcontainer">
-            <h3 className="SuccessAboutH3">Boyut: {/* <span>{pizzaDetails.boyut}</span> */}</h3>
-            <h3 className="SuccessAboutH3" >Hamur: {/* <span>{pizzaDetails.hamur}</span> */}</h3>
-            <h3 className="SuccessAboutH3">Ek Malzemeler: {/* <span>{pizzaDetails.ekler}</span> */}</h3>
-            </div>
-        </div>
-    );
+  const { pizzaDetails } = useParams();
+  return (
+    <div className="success-about-container">
+      <h2 className="SuccessAboutH2">Pizza İsmi</h2>
+      <div className="success-about-smallcontainer">
+        <h3 className="SuccessAboutH3">
+          Boyut: {/* <span>{pizzaDetails.boyut}</span> */}
+        </h3>
+        <h3 className="SuccessAboutH3">
+          Hamur: {/* <span>{pizzaDetails.hamur}</span> */}
+        </h3>
+        <h3 className="SuccessAboutH3">
+          Ek Malzemeler: {/* <span>{pizzaDetails.ekler}</span> */}
+        </h3>
+      </div>
+    </div>
+  );
 };
 export default SuccessAbout;
